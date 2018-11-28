47112755c7a066dbd7c7282bf6524b1c4e65fbac
ChloeHelen <cmingay@fastmail.fm>
2018-11-28 13:08:52 +0000

    Returns Fizz if Divisible by Three

---
+++ FizzBuzz-C-/FizzBuzz-C--tests/FizzBuzzTests.cs
@@ -0,0 +1,28 @@
+﻿using FizzBuzz_C_;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace FizzBuzz_C__tests
+{
+    public class FizzBuzzTests
+    {
+
+        [Fact]
+        public void DivisibleByThreeReturnsFizz()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                FizzBuzz.DivisibleByThree(3);
+
+                string expected = string.Format("Fizz{0}", Environment.NewLine);
+                Assert.Equal(expected, sw.ToString());
+            }
+        }
+
+    }
+}
---
+++ FizzBuzz-C-/FizzBuzz-C-/FizzBuzz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FizzBuzz_C_
+{
+    public class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        public static void DivisibleByThree(int num)
+        {
+            if (num % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+         
+        }
+    }
+}
